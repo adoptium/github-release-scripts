42ed12418648aba4164459512a4a77e2ceb5de11
John Oliver <johno@insightfullogic.com>
2018-10-02 19:06:33 +0100

    Support new naming scheme

--- sbin/Release.sh
+++ sbin/Release.sh
@@ -15,8 +15,13 @@
 
 npm install
 
+versionRegex="[[:digit:]]{2}_[[:digit:]]{2}"
 timestampRegex="[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}-[[:digit:]]{2}-[[:digit:]]{2}"
-regex="OpenJDK([[:digit:]]+)U?(-jre)?_([[:alnum:]]+)_([[:alnum:]]+)_([[:alnum:]]+).*_($timestampRegex).(tar.gz|zip)";
+
+#
+#      OpenJDK 8U_             -jdk         x64_           Linux_         hotspot_         2018-06-15-10-10                       .tar.gz
+#      OpenJDK 11_             -jdk         x64_           Linux_         hotspot_         11_28                                  .tar.gz
+regex="OpenJDK([[:digit:]]+)U?(-jre|-jdk)_([[:alnum:]]+)_([[:alnum:]]+)_([[:alnum:]]+).*_($timestampRegex|$versionRegex).(tar.gz|zip)";
 regexArchivesOnly="${regex}$";
 
 
@@ -38,21 +43,26 @@ do
   then
     newName=$(echo "${file}" | sed -r "s/${timestampRegex}/$TIMESTAMP/")
 
-    # Rename archive and checksum file with now timestamp
-    echo "Renaming ${file} to ${newName}"
-    mv "${file}" "${newName}"
-    mv "${file}.sha256.txt" "${newName}.sha256.txt"
+    if [ "${file}" != "${newName}" ]; then
+      # Rename archive and checksum file with now timestamp
+      echo "Renaming ${file} to ${newName}"
+      mv "${file}" "${newName}"
+      mv "${file}.sha256.txt" "${newName}.sha256.txt"
 
-    # Fix checksum file name
-    sed -i -r "s/^([0-9a-fA-F ]+).*/\1${newName}/g" "${newName}.sha256.txt"
+      # Fix checksum file name
+      sed -i -r "s/^([0-9a-fA-F ]+).*/\1${newName}/g" "${newName}.sha256.txt"
+    fi
+
 
     FILE_VERSION=${BASH_REMATCH[1]};
+    FILE_TYPE=${BASH_REMATCH[2]};
     FILE_ARCH=${BASH_REMATCH[3]};
     FILE_OS=${BASH_REMATCH[4]};
     FILE_VARIANT=${BASH_REMATCH[5]};
+    FILE_TS_OR_VERSION=${BASH_REMATCH[6]};
     FILE_EXTENSION=${BASH_REMATCH[7]};
 
-    echo "version:${FILE_VERSION} arch:${FILE_ARCH} os:${FILE_OS} variant:${FILE_VARIANT} timestamp:${TIMESTAMP} extension:${FILE_EXTENSION}";
+    echo "version:${FILE_VERSION} type: ${FILE_TYPE} arch:${FILE_ARCH} os:${FILE_OS} variant:${FILE_VARIANT} timestamp or version:${FILE_TS_OR_VERSION} timestamp:${TIMESTAMP} extension:${FILE_EXTENSION}";
   fi
 done
 
