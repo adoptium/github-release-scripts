a296f71c5aa4a191efb4f659537a9e7f7e69fee4
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-03-26 13:07:57 +0000

    Move release script over to Groovy (#51)
    * Fix logic so that a nightly can override the publish name
    
    * Test using groovy
    
    * Add wrapper
    
    * Fixes from review
    
    * Fix checksum path

---
+++ adopt-github-release/src/main/groovy/net/adoptopenjdk/release/UploadFiles.groovy
@@ -0,0 +1,115 @@
+package net.adoptopenjdk.release
+
+import groovy.cli.picocli.CliBuilder
+import groovy.cli.picocli.OptionAccessor
+import groovy.transform.CompileStatic
+import org.kohsuke.github.GHAsset
+import org.kohsuke.github.GHRelease
+import org.kohsuke.github.GHRepository
+import org.kohsuke.github.GitHub
+
+import java.nio.file.Files
+
+@CompileStatic
+class UploadAdoptReleaseFiles {
+
+    private final String tag
+    private final String description
+    private final boolean release
+    private final List<File> files
+    private final String version
+
+    UploadAdoptReleaseFiles(String tag, String description, boolean release, String version, List<File> files) {
+        this.tag = tag
+        this.description = description
+        this.release = release
+        this.files = files
+        this.version = version
+    }
+
+    void release() {
+        GHRepository repo = getRepo()
+        GHRelease release = getRelease(repo)
+        uploadFiles(release)
+    }
+
+    private GHRepository getRepo() {
+        String token = System.getenv("GITHUB_TOKEN")
+        if (token == null) {
+            System.err.println("Could not find GITHUB_TOKEN")
+            System.exit(1)
+        }
+
+        GitHub github = GitHub.connectUsingOAuth(token)
+        return github.getRepository("AdoptOpenJDK/open${version}-binaries")
+    }
+
+    private void uploadFiles(GHRelease release) {
+        List<GHAsset> assets = release.getAssets()
+        files.each { file ->
+            // Delete existing asset
+            assets
+                    .find({ it.name == file.name })
+                    .each { GHAsset existing ->
+                println("Updating ${existing.name}")
+                existing.delete()
+            }
+
+            println("Uploading ${file.name}")
+            release.uploadAsset(file, Files.probeContentType(file.toPath()))
+        }
+    }
+
+    private GHRelease getRelease(GHRepository repo) {
+        GHRelease release = repo
+                .getReleaseByTagName(tag)
+
+        if (release == null) {
+            release = repo
+                    .createRelease(tag)
+                    .body(description)
+                    .name(tag)
+                    .prerelease(!this.release)
+                    .create()
+        }
+        return release
+    }
+}
+
+
+static void main(String[] args) {
+    OptionAccessor options = parseArgs(args)
+
+    List<File> files = options.arguments()
+            .collect { new File(it) }
+
+    new UploadAdoptReleaseFiles(
+            options.t,
+            options.d,
+            options.r,
+            options.v,
+            files,
+    ).release()
+}
+
+private OptionAccessor parseArgs(String[] args) {
+
+    CliBuilder cliBuilder = new CliBuilder()
+
+    cliBuilder
+            .with {
+        v longOpt: 'version', type: String, args: 1, 'JDK version'
+        t longOpt: 'tag', type: String, args: 1, 'Tag name'
+        d longOpt: 'description', type: String, args: 1, 'Release description'
+        r longOpt: 'release', 'Is a release build'
+        h longOpt: 'help', 'Show usage information'
+    }
+
+    def options = cliBuilder.parse(args)
+    if (options.v && options.t && options.d) {
+        return options
+    }
+    cliBuilder.usage()
+    System.exit(1)
+    return null
+}
--- sbin/Release.sh
+++ sbin/Release.sh
@@ -13,8 +13,6 @@
 # limitations under the License.
 #
 
-npm install
-
 #             11 style version           | 8 Style                     | 9/10 style
 versionRegex="[[:digit:]]{2}_[[:digit:]]+|8u[[:digit:]]+-?(b[[:digit:]]+|ga)|[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+_[[:digit:]]+"
 timestampRegex="[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}-[[:digit:]]{2}-[[:digit:]]{2}"
@@ -52,7 +50,8 @@ do
     fi
 
     # Fix checksum file name
-    sed -i -r "s/^([0-9a-fA-F ]+).*/\1${newName}/g" "${newName}.sha256.txt"
+    strippedFileName=$(echo "${newName}" | sed -r "s/.+\\///g")
+    sed -i -r "s/^([0-9a-fA-F ]+).*/\1${strippedFileName}/g" "${newName}.sha256.txt"
 
     FILE_VERSION=${BASH_REMATCH[1]};
     FILE_TYPE=${BASH_REMATCH[2]};
@@ -70,12 +69,14 @@ files=`ls $PWD/OpenJDK*{.tar.gz,.sha256.txt,.zip,.pkg,.msi} | sed -e ':a' -e 'N'
 
 echo "Release: $RELEASE"
 
+RELEASE_OPTION=""
 if [ "$RELEASE" == "true" ]; then
   description="Official Release of $TAG"
+  RELEASE_OPTION="--release"
 else
   description="Nightly Build of $TAG"
 fi
 
-node upload.js --files $files --tag ${TAG} --description "${description}" --release "$RELEASE"
-
-node app.js
+cd adopt-github-release
+chmod +x gradlew
+./gradlew run --args="--version \"${VERSION}\" --tag \"${TAG}\" --description \"${description}\" $RELEASE_OPTION $files"
