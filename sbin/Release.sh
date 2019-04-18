7bf38e6992dc41e52fe2fd7f6a0e5ab4e696d2e1
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-04-18 11:04:32 +0100

    Be less exact when looking for archives. Move json file when renamed (#56)
    * Temp bodge to fix 212 file names
    
    * Be less exact on archive matching
    
    * remove comment

--- sbin/Release.sh
+++ sbin/Release.sh
@@ -13,14 +13,12 @@
 # limitations under the License.
 #
 
-#             11 style version           | 8 Style                     | 9/10 style
-versionRegex="[[:digit:]]{2}_[[:digit:]]+|8u[[:digit:]]+-?(b[[:digit:]]+|ga)|[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+_[[:digit:]]+"
 timestampRegex="[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}-[[:digit:]]{2}-[[:digit:]]{2}"
 
 # IF YOU ARE MODIFYING THIS THEN THE FILE MATCHING IS PROBABLY WRONG, MAKE SURE openjdk-api, v2.js IS UPDATED TOO
 #      OpenJDK 8U_             -jdk        x64_           Linux_         hotspot_         2018-06-15-10-10                .tar.gz
 #      OpenJDK 11_             -jdk        x64_           Linux_         hotspot_         11_28                           .tar.gz
-regex="OpenJDK([[:digit:]]+)U?(-jre|-jdk)_([[:alnum:]\-]+)_([[:alnum:]]+)_([[:alnum:]]+).*_($timestampRegex|$versionRegex).(tar.gz|zip|pkg|msi)";
+regex="OpenJDK([[:digit:]]+)U?(-jre|-jdk)_([[:alnum:]\-]+)_([[:alnum:]]+)_([[:alnum:]]+).*\.(tar\.gz|zip|pkg|msi)";
 regexArchivesOnly="${regex}$";
 
 if [ -z "${TAG}" ]; then
@@ -50,6 +48,7 @@ do
       echo "Renaming ${file} to ${newName}"
       mv "${file}" "${newName}"
       mv "${file}.sha256.txt" "${newName}.sha256.txt"
+      mv "${file}.json" "${newName}.json"
     fi
 
     # Fix checksum file name
