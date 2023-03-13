d2ef5ba1a4145b8aa7e8f45e8c7827b796e7df36
Andrew Leonard <anleonar@redhat.com>
2023-03-13 19:08:53 +0000

    Ensure Release.sh does not exit if .sig/json/sha256.txt is not present or being selected
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- sbin/Release.sh
+++ sbin/Release.sh
@@ -142,11 +142,8 @@ if [ "$UPLOAD_TESTRESULTS_ONLY" == "false" ]; then
     fi
   done
   # Grab the list of files to upload
-  # TODO - shellcheck (SC2012) tells us that using find is better than ls here.
   # NOTE: If adding something here you may need to change the EXPECTED values in releaseCheck.sh
-  echo "HERE1"
-  files=$(ls "$PWD"/OpenJDK*{.tar.gz,.sha256.txt,.zip,.pkg,.msi,.json,*.sig} | grep -v makefailurelogs | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g')
-  echo "HERE2"
+  files=$(find $PWD \( -name "*.tar.gz" -o -name "*.sha256.txt" -o -name "*.zip" -o -name "*.pkg" -o -name "*.msi" -o -name "*.json" -o -name "*.sig" \) -exec basename {} \; | tr '\n' ' ')
 else 
   #TODO: enhance to a general file name - update groovy release() - case ~/.*AQAvitTapFiles.*/: "adopt"; break;
   files=$(ls "$PWD"/AQAvitTapFiles.tar.gz)
