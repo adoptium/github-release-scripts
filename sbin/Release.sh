95f0951984fb2e1c5dcd9c4d486c2e25506664ab
Andrew Leonard <31470007+andrew-m-leonard@users.noreply.github.com>
2025-05-08 13:18:51 +0100

    Add release publish support for jmods package (#194)
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- sbin/Release.sh
+++ sbin/Release.sh
@@ -40,9 +40,10 @@ timestampRegex="[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}-[[:digit:]]{2}-[[:d
 #      OpenJDK 11_             -jdk        x64_           linux_         _fast_startup_   11_28                           .tar.gz
 #      OpenJDK                 -debugimage aarch64_       linux_         hotspot_         2023-02-16-12-32                .tar.gz
 #      OpenJDK 17U_            -jdk        x64_           linux_         hotspot_         17.0.10_5-ea                    .tar.gz
+#      OpenJDK                 -jmods      aarch64_       alpine-linux_  hotspot_         25_21-ea                        .tar.gz
 #
-#             (version     )  (type                                                                                           ) (arch           ) (os             ) (variant         ) (timestamp     or version        )  (extension          )
-regex="OpenJDK([[:digit:]]*)U?(-jre|-jdk|-debugimage|-static-libs-glibc|-static-libs|-static-libs-musl|-testimage|-jdk-sources)_([[:alnum:]\-]+_)?([[:alnum:]\-]+_)?([[:alnum:]\-_]+_)?([[:digit:]\-]+|[[:alnum:]\._\-]+)\.(tar\.gz|zip|pkg|msi)";
+#             (version     )  (type                                                                                                  ) (arch           ) (os             ) (variant         ) (timestamp     or version        )  (extension          )
+regex="OpenJDK([[:digit:]]*)U?(-jmods|-jre|-jdk|-debugimage|-static-libs-glibc|-static-libs|-static-libs-musl|-testimage|-jdk-sources)_([[:alnum:]\-]+_)?([[:alnum:]\-]+_)?([[:alnum:]\-_]+_)?([[:digit:]\-]+|[[:alnum:]\._\-]+)\.(tar\.gz|zip|pkg|msi)";
 
 regexArchivesOnly="${regex}$";
 
